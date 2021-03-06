0b070922169d5cb258d10ef439712bc87a803905
ryanchangx <ryanchangx@gmail.com>
2021-03-05 19:22:50 -0800

    Begin implementing room.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 #include ",,/header/room.h"
+#include "../header/character.hpp"
+#include "../header/weapon.hpp"
+#include "../header/factory.hpp"
+
 
 int main(){
-	
+	Battle* Battle1 = new Battle();
+	Battle1->fight();
+	return 0;	
 }
