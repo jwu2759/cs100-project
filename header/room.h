e7e1a43d59b9cf251d7c717cccf12ecafae292c6
ryanchangx <ryanchangx@gmail.com>
2021-03-09 02:19:15 -0800

    Added the entire story and formatting. Also created AllyBattle rooms and Wish room. Constructor for all rooms now take in another room.

--- header/room.h
+++ header/room.h
@@ -9,12 +9,15 @@
 using namespace std;
 
 class Room{
-	private:
+	protected:
 		Room* nextRoom = nullptr;
 	public:
 		Room();
 		Room(Room* r);
-		virtual ~Room() = default;
+		virtual ~Room(){
+			if(nextRoom)
+				delete nextRoom;
+		}
 		void setNext(Room* r);
 		Room* getNext();
 		virtual bool execute(Player* p, Ally* a) = 0;
@@ -24,7 +27,6 @@ class Battle : public Room{
 	private:
 		vector<Enemy*> enemies;
 		bool clear();	//returns if room is cleared, if enemies is empty return true
-		string BattleText;	//add more strings as needed for Battle.	
 		void outCurrent(){
 			cout << "- ENEMIES REMAINING -" << endl;
 			for(int i = 0; i < enemies.size(); ++i){
@@ -44,15 +46,20 @@ class Battle : public Room{
 			sleep(1);	
 			int allyDamage = a->weaponDamage();
 			a->upgradeWeapon();
-			cout << a->getName() << "'s weapon has been upgraded! " << allyDamage << "->" << a->weaponDamage() << "ATK" << endl;
+			cout << a->getName() << "'s weapon has been upgraded! " << allyDamage << "->" << a->weaponDamage() << "ATK" << endl;			
 			sleep(1);	
 			int healPrev = a->getHealBase();
 			a->upgradeHeal();
 			cout << a->getName() << "'s ability healing has been increased! " << healPrev << "->" << a->getHealBase() << "HP" << endl;
-			sleep(4);	
+			do{
+				cout << "";
+			} while(cin.get() != '\n');
+			do{
+				cout << '\n' << "Press ENTER to continue...";
+			} while(cin.get() != '\n');
 		}
 	public:
-		Battle(int);
+		Battle(int, Room*);
 		~Battle();
 		bool fight(Player* p, Ally* a);	//call clear in a while loop, in while loop, sequence of fighting happens
 		bool execute(Player* p, Ally* a){
@@ -79,20 +86,43 @@ class Battle : public Room{
 		}
 };
 
-class Story : public Room{
-	private:
-		string storyText;
-	public:
-		Story(const string& story){
-			storyText = story;
-		}	
-		const string& getStory(){
-			return storyText;
+class AllyBattle : public Room{
+		void levelUp(Player* p){
+			int prevDamage = p->weaponDamage();
+			p->upgradeWeapon();
+			cout << p->getName() << "'s weapon has been upgraded! " << prevDamage << "->" << p->weaponDamage() << "ATK" << endl;
+			sleep(1);	
+			int prevAbi = p->getAbiBase();
+			p->upgradeAbi();
+			cout << p->getName() << "'s cleave damage has been increased! " << prevAbi << "->" << p->getAbiBase() << "ATK" << endl;
+			do{
+				cout << "";
+			} while(cin.get() != '\n');
+			do{
+				cout << '\n' << "Press ENTER to continue...";
+			} while(cin.get() != '\n');
 		}
+	public:
+		AllyBattle(Room* r){
+			nextRoom = r;
+		}
+		bool fight(Player* p, Ally* a);
 		bool execute(Player* p, Ally* a){
+			bool f = fight(p,a);
+			levelUp(p);
 			system("clear");
-			string temp;
-			cout << getStory() << endl;
+			cout << "*You drive your blade deep into the throat of " << a->getName() << "*" << endl;
+			sleep(2);
+			cout << "You let out a horrendous cry as you strangle the remaining fragments of life your best friend has left" << endl << endl;
+			sleep(1);
+			cout << a->getName() << ":" << endl;
+			cout << "\t..." << endl;
+			sleep(2);
+			cout << "\t\t.." << endl;
+			sleep(2);
+			cout << "\t\t\t." << endl << endl;
+			sleep(3);
+			cout << p->getName() << ": GIVE ME MY WISH!!! GIVE ME " << p->pWish() << "!!!!!" << endl;
 			do{
 				cout << '\n' << "Press ENTER to continue...";
 			} while(cin.get() != '\n');
@@ -100,13 +130,72 @@ class Story : public Room{
 		}
 };
 
+class Story : public Room{
+	private:
+		vector<string> storyText;
+	public:
+		Story(const vector<string>& story, Room* r){
+			storyText = story;
+			nextRoom = r;
+		}	
+		const vector<string>& getStory(){
+			return storyText;
+		}
+		virtual bool execute(Player* p, Ally* a){
+			vector<string> temp = storyText;
+			system("clear");
+			for(int i = 0; i < temp.size(); ++i){
+				cout << temp.at(i) << endl;
+				sleep(1);
+			}
+			do{
+				cout << '\n' << "Press ENTER to continue...";
+			} while(cin.get() != '\n');
+			return true;
+		}
+};
+
+class Wish : public Room{
+	//Wish ONLY gets input
+	string wishText;
+	void getWish(Player* p){
+		string temp;
+		cout << "Type your wish: ";
+		getline(cin,temp);
+		p->setWish(temp);
+	}
+	public:
+	Wish(const string& s, Room* r){
+		nextRoom = r;
+		wishText = s;
+	}
+	bool execute(Player* p, Ally* a){
+		system("clear");
+		cout << wishText << endl;
+		sleep(1);
+		getWish(p);
+		string temp = p->getName() + " (to yourself): Right..." + p->pWish() + " is my wish...";
+		cout << temp << endl;
+		do{
+			cout << '\n' << "Press ENTER to continue...";
+		} while(cin.get() != '\n');
+		return true;
+	}
+};
+
 class Shop : public Room{
 	private:
 		void inventory(Player* p);
 	public:
+		Shop(Room* r){
+			nextRoom = r;
+		}
 		void display(Player* p);
 		bool execute(Player* p, Ally* a){
 			display(p);
+			do{
+				cout << "";
+			} while(cin.get() != '\n');
 			return true;
 		}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,56 @@ int main(){
 	srand(time(0));
 	Player* user = new Player("Kanon", 1);
 	Ally* comp = new Ally("Qroobz", 2);
-	Story* Story1 = new Story("Wow good story this is fantastic.");
-	Battle* Battle1 = new Battle(3);
-	Story* Story2 = new Story("Nvm this story sux 0/10 would not play again.");
-	Shop* shop = new Shop();
-	Battle* Battle2 = new Battle(4);
-	RoomIter iter(Story1);
-	Story1->setNext(Battle1);
-	Battle1->setNext(Story2);
-	Story2->setNext(shop);
-	shop->setNext(Battle2);
+	Story* game = new Story(
+		{"March 14, 4801 AE: Eternal darkness has ravaged the skies.", 
+		"The sun hasn’t risen in months. The corn stalks in the field have shriveled into twigs.\n",
+		"At the bottom of the land, you and " + comp->getName() + " stare up at the void in the sky.",
+		"Despite the vastness of the void, The Mountain's magnitude seemed to dominate the world.",
+		"Being ten thousand meters tall, The Mountain loomed over the small mountain village where you currently stand.",
+		"Legend has it that The Mountain could grant any one wish for every brave soul who dared escalate the gargantuan elevation.",
+		"Although countless climbers have started the climb to peak, not a single soul has returned to tell the tale. \n", 
+		comp->getName() + " coughing incessantly, " + comp->getName() + " is getting progressively more ill every day.  Just like their mother, who had passed away from the same fatal disease. \n",
+		comp->getName() + ": Come on " + user->getName() + ", we can’t just stay here, do you really just want to rot away like this?",
+		"I’ve already lost my mom to this world...",
+		"The longer I sit around and do nothing, the sooner my whole family and entire village will die of starvation.",
+		"I can’t let that happen. We have nothing left to lose! Don’t you want your wish too?"}, 
+	new Wish(user->getName() + " (to yourself): What was my wish?", 
+	new Story({user->getName() + ": You know " + comp->getName() + ", you’re right, how could I give up on that! I’ve wanted this for as long as I can remember...",
+		user->getName() + ": Okay... To the Mountain... as long as we got each other's backs, we can take anything on."}, 
+	new Story({"***THE JOURNEY TO THE MOUNTAIN HAS BEGUN***"}, 
+	new Story({user->getName() +  ": Hey " + comp->getName() + ", careful. Look ahead... You see those other guys over there? They’re definitely bandits.", 
+	"We need to be on guard, so get your weapon ready. This’ll be our first fight. You set?",
+	comp->getName() + ": Yeah, I’m ready. I’ll do whatever it takes to save my village."},
+ 	new Battle(3,
+	new Story({comp->getName() + ": *Coughs* Excuse me...Oh check that out, it’s a shop!"},
+	new Shop(
+	new Story({comp->getName() + ": That was a lot tougher than I thought it would be. I now understand how so many people have died before us.",
+	user->getName() + ": Definitely. I’m glad we were able to take that large group of enemies down, it shows that we have a chance of making it to the top!",
+	"Though far off, we’ve come a long way already in regards to reaching the top.", 
+	user->getName() + ": Woah, " + comp->getName() + ", are you alright? You don’t look so good...You’re coughing out blood.",
+	comp->getName() + ": Huh...My Mother started coughing out blood on the day she passed..." + user->getName() + ", we need to get to the top so I can get that one wish.",
+	user->getName() + ": Planning to wish for good health?",
+	comp->getName() + ": Nah...I might not make it, but I have to make sure my village does.",
+	"I need to use that wish to provide food for everyone.", 
+	user->getName() + ": WATCH OUT! SOMEONE’S BEHIND YOU!"},
+	new Battle(4, 
+	new Shop(
+	new Story({comp->getName() + ": The top is so close, we can do this " + user->getName() + "!", 
+	user->getName() + ": Yeah! I see it! I can’t believe we made it this far. We can secure our wishes!",
+	"All we have to do is clear these last enemies, then we’ll be at the top!"}, 
+	new Battle(5, 
+	new Story({comp->getName() + ": Oh my god, " + user->getName() + " we made it to the top. But...I don’t see anything here?? Do you?", 
+	"\t" + user->getName() + " There’s a note, hammered down in the ground over there.",
+	"The note reads:\n\t**IN ORDER TO GAIN SOMETHING, YOU MUST LOSE SOMETHING**",
+	user->getName() + " realizes that the only way to get their wish is to kill someone...,", 
+	"*" + user->getName() + " turns their head towards " + comp->getName() + " with murderous intent*\n"}, 
+	new AllyBattle(
+	new Story({"After waiting nearly a week at the cold Mountain Top, nothing had happened...",
+	"No wish was granted. The stories, the legends, the myths, the rumors were all lies. There was never any wish.",
+	"Eternal darkness has ravaged the skies. The sun hasn’t risen in months.",
+	"The corn stalks in the field have shriveled into twigs.",
+	"This never-ending cycle of despair continues..."}, nullptr) ) ) ) ) ) ) ) ) ) ) ) ) ) ) );
+	RoomIter iter(game);
 	while(!iter.isDone()){
 		if(!iter.execute(user,comp))
 			break;
@@ -26,10 +66,6 @@ int main(){
 	}
 	delete user;
 	delete comp;
-	delete Story1;
-	delete Battle1;
-	delete Story2;
-	delete shop;
-	delete Battle2;
+	delete game;
 	return 0;	
 }
